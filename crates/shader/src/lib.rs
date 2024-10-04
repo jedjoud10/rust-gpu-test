203bdf42f99c18df091fb84be8de303996569945
Jedjoud10 <jribijed6@gmail.com>
2024-10-04 15:17:00 -0400

    Sus

--- crates/shader/src/lib.rs
+++ crates/shader/src/lib.rs
@@ -6,6 +6,7 @@
 mod intersection;
 mod raymarch;
 mod blit;
+mod lighting;
 pub use blit::*;
 pub use intersection::*;
 pub use raymarch::*;
@@ -36,13 +37,6 @@ pub struct ShaderConstants {
     pub height: f32,
 }
 
-fn dist(pos: Vec3) -> f32 {
-    let floor = pos.y;
-    let sphere = pos.length() - 1.0f32;
-
-    floor.min(sphere)
-}
-
 #[spirv(compute(threads(32, 32, 1)))]
 pub unsafe fn test(
     #[spirv(global_invocation_id)] id: UVec3,
@@ -54,46 +48,12 @@ pub unsafe fn test(
     coords *= 2.0f32;
     coords.y = -coords.y;
 
-    let mut ray_dir = constants.proj_matrix.inverse().mul_vec4(vec4(coords.x, coords.y, -1f32, 1f32));
-    ray_dir.w = 0f32;
-    let ray_dir = constants.view_matrix.inverse().mul_vec4(ray_dir).xyz().normalize();
-    let inv_dir = ray_dir.recip();
+    let mut _dir = constants.proj_matrix.inverse().mul_vec4(vec4(coords.x, coords.y, -1f32, 1f32));
+    _dir.w = 0f32;
+    let dir = constants.view_matrix.inverse().mul_vec4(_dir).xyz().normalize();
 
-    let mut color = Vec3::ZERO;
-    let mut pos = constants.position.xyz() + ray_dir * 0.2f32;
-    //color = ray_dir;
+    let output = raymarch::raymarch(constants.position.xyz(), dir);
+    let output = lighting::light(output, dir);
 
-    for x in 0..64 {
-        let min = (pos * 1f32).floor();
-        let max = (pos * 1f32).ceil();
-
-        let int = intersection(pos, inv_dir, min, max);
-        let dist = f32::max(int.y, 0.001f32);
-        pos += ray_dir * dist;
-
-        let mut face = 0u32;
-        intersection_faces(pos + ray_dir * 0.01f32, -inv_dir, min, max, &mut face);
-
-        /* 
-        if (f32::sin(min.x * 1.00f32) * 6.0f32 > min.y) {
-            color = box_normal(face, -ray_dir);
-            break;
-        }
-        */
-
-        /*
-        let dis = dist(pos);
-        pos += ray_dir * dis;
-
-        if dis < 0.001 {
-            //color = pos;
-            break;
-        }
-        */
-
-        color.x = x as f32 / 64.0f32;
-    }
-
-    //image.write(id.xy(), vec4(coords.y, 0f32, 0f32, 1f32));
-    image.write(id.xy(), Vec4::from((color, 1f32)));
+    image.write(id.xy(), Vec4::from((output, 1f32)));
 }
---
+++ crates/shader/src/raymarch.rs
@@ -0,0 +1,73 @@
+use glam::Vec3;
+use spirv_std::float::*;
+use spirv_std::num_traits::*;
+use spirv_std::number::*;
+use spirv_std::num_traits::float::*;
+use spirv_std::num_traits::real::*;
+
+
+use crate::{box_normal, intersection, intersection_faces};
+
+pub struct RaymarchOutput {
+    pub position: Vec3,
+    pub normal: Vec3,
+    pub hit: bool,
+}
+
+fn indeed(pos: Vec3) -> bool {
+    let mut sum = 0f32;
+
+    for i in 1..5 {
+        let scale = f32::powf(2f32, i as f32);
+        let amplitude = f32::powf(0.5f32, i as f32);
+        sum += f32::sin(pos.x * 0.70f32 * scale) * amplitude * 6f32;
+    }
+    
+    sum > pos.y
+}
+
+pub fn raymarch(
+    ray_start: Vec3,
+    ray_dir: Vec3,
+) -> RaymarchOutput {
+    let inv_dir = ray_dir.recip();
+    let mut pos = ray_start + ray_dir * 0.2f32;
+
+    for x in 0..64 {
+        let min = (pos * 1f32).floor();
+        let max = (pos * 1f32).ceil();
+
+        let int = intersection(pos, inv_dir, min, max);
+        let dist = f32::max(int.y, 0.001f32);
+        pos += ray_dir * dist;
+
+        let mut face = 0u32;
+        intersection_faces(pos + ray_dir * 0.01f32, -inv_dir, min, max, &mut face);
+
+        if indeed(min) {
+            let normal = box_normal(face, -ray_dir);
+            return RaymarchOutput {
+                position: pos,
+                normal,
+                hit: true,
+            };
+        }
+
+        /*
+        let dis = dist(pos);
+        pos += ray_dir * dis;
+
+        if dis < 0.001 {
+            //color = pos;
+            break;
+        }
+        */
+
+    }
+
+    return RaymarchOutput {
+        position: Vec3::ZERO,
+        normal: Vec3::ZERO,
+        hit: false,
+    };
+}
